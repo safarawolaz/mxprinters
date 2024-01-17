63355f49204dfe13426d88f6e455eb816d0a991b
safarawolaz <safarawolaz@yahoo.com>
2024-01-17 15:29:23 +0200

    social proof section

--- app/langingPage/SocialProof.tsx
+++ app/langingPage/SocialProof.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 
 const SocialProof = () => {
   return (
-    <section className='w-full min-h-[50vh] bg-white  flex flex-col justify-center items-center px-10 py-10 sm:py-20'>
-        <h3 className='text-3xl sm:text-4xl font-bold mt-8'>Brands That Trust Our Products</h3>
+    <section className='w-full min-h-[50vh] bg-white  flex flex-col justify-center items-center  px-10 py-10 sm:py-20'>
+        <h3 className='text-3xl sm:text-4xl font-bold mt-8 '>Brands That Trust Our Products</h3>
         <p className='text-base text-muted-foreground mt-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, mollitia?</p>
-        <div className="w-full mt-10 flex flex-col sm:flex-row justify-center items-center sm:justify-around gap-4">
-            <div className="h-32 w-32 bg-gray-500  rounded-full"></div>
-            <div className="h-32 w-32 bg-gray-500 rounded-full"></div>
-            <div className="h-32 w-32 bg-gray-500 rounded-full"></div>
-            <div className="h-32 w-32 bg-gray-500 rounded-full"></div>
-            <div className="h-32 w-32 bg-gray-500 rounded-full"></div>
+        <div className="w-full mt-10 grid grid-cols-2 md:grid-cols-4 place-items-center gap-4 ">
+            <div className="h-32 w-32 bg-gray-500  rounded-full">
+            </div>
+            <div className="h-32 w-32 bg-gray-500 rounded-full">
+            </div>
+            <div className="h-32 w-32 bg-gray-500 rounded-full">
+            </div>
+            <div className="h-32 w-32 bg-gray-500 rounded-full">
+            </div>
         </div>
     </section>
   )
