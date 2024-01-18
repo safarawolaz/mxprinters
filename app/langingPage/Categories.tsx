96eb25699eb43b729a3b077a3efea5658e565b9f
safarawolaz <safarawolaz@yahoo.com>
2024-01-18 04:01:49 +0200

    animations and images added

--- app/langingPage/Categories.tsx
+++ app/langingPage/Categories.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { ArrowRight, LucideBookOpen } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Categories = () => {
@@ -16,7 +17,9 @@ const Categories = () => {
           <h3 className='text-2xl font-bold text-center'>Banners</h3>
           <p className='text-base text-muted-foreground text-center mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           <div className="mt-6  flex justify-center">
-            <div className="w-44 h-44 bg-gray-400 rounded-full"></div>
+            <div className="w-44 h-44 bg-gray-400 rounded-full relative overflow-hidden">
+              <Image src='/categories-1.jpg' fill alt='blog image' objectFit='cover' />
+            </div>
           </div>
           <div className='flex justify-center mt-6'>
             <Button className='text-sm  font-bold rounded-full w-max tracking-widest' size='lg' variant='ghost'><ArrowRight className='w-5 h-5 text-orange-600 mr-2' />
@@ -24,11 +27,13 @@ const Categories = () => {
             </Button>
           </div>
         </div>
-        <div className="bg-white p-10 rounded-3xl">
+        <div className="bg-white p-10 rounded-3xl hover:translate-y-[-16px] transition-all  duration-500 cursor-pointer">
           <h3 className='text-2xl font-bold text-center'>Booklets</h3>
           <p className='text-base text-muted-foreground text-center mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           <div className="mt-6  flex justify-center">
-            <div className="w-44 h-44 bg-gray-400 rounded-full"></div>
+            <div className="w-44 h-44 bg-gray-400 rounded-full relative overflow-hidden">
+              <Image src='/categories-2.jpg' fill alt='blog image' objectFit='cover' />
+            </div>
           </div>
           <div className='flex justify-center mt-6'>
             <Button className='text-sm  font-bold rounded-full w-max tracking-widest' size='lg' variant='ghost'><ArrowRight className='w-5 h-5 text-orange-600 mr-2' />
@@ -36,11 +41,13 @@ const Categories = () => {
             </Button>
           </div>
         </div>
-        <div className="bg-white p-10 rounded-3xl">
+        <div className="bg-white p-10 rounded-3xl hover:translate-y-[-16px] transition-all  duration-500 cursor-pointer">
           <h3 className='text-2xl font-bold text-center'>Business Cards</h3>
           <p className='text-base text-muted-foreground text-center mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           <div className="mt-6  flex justify-center">
-            <div className="w-44 h-44 bg-gray-400 rounded-full"></div>
+            <div className="w-44 h-44 bg-gray-400 rounded-full relative overflow-hidden">
+              <Image src='/categories-3.jpg' fill alt='blog image' objectFit='cover' />
+            </div>
           </div>
           <div className='flex justify-center mt-6'>
             <Button className='text-sm  font-bold rounded-full w-max tracking-widest' size='lg' variant='ghost'><ArrowRight className='w-5 h-5 text-orange-600 mr-2' />
@@ -48,11 +55,13 @@ const Categories = () => {
             </Button>
           </div>
         </div>
-        <div className="bg-white p-10 rounded-3xl">
+        <div className="bg-white p-10 rounded-3xl hover:translate-y-[-16px] transition-all  duration-500 cursor-pointer">
           <h3 className='text-2xl font-bold text-center'>Calenders</h3>
           <p className='text-base text-muted-foreground text-center mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           <div className="mt-6  flex justify-center">
-            <div className="w-44 h-44 bg-gray-400 rounded-full"></div>
+            <div className="w-44 h-44 bg-gray-400 rounded-full relative overflow-hidden">
+              <Image src='/categories-4.jpg' fill alt='blog image' objectFit='cover' />
+            </div>
           </div>
           <div className='flex justify-center mt-6'>
             <Button className='text-sm  font-bold rounded-full w-max tracking-widest' size='lg' variant='ghost'><ArrowRight className='w-5 h-5 text-orange-600 mr-2' />
