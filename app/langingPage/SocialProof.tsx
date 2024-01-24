0938da21bc16fec6ad5b7f5f17d1ce8b32f6c0e4
safarawolaz <safarawolaz@yahoo.com>
2024-01-24 04:03:41 +0200

    adjustments margins

--- app/langingPage/SocialProof.tsx
+++ app/langingPage/SocialProof.tsx
@@ -7,7 +7,7 @@ const SocialProof = () => {
     <section className='w-full min-h-[50vh] bg-white  flex flex-col justify-center items-center  px-10 py-10 sm:py-20'>
         <h3 className='text-3xl text-[#0e0322] sm:text-4xl font-bold mt-8 '>Brands That Trust Our Products</h3>
         <p className='text-base text-muted-foreground mt-8'>Here are some of the companies who value our services</p>
-        <div className="w-full mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center gap-4 ">
+        <div className="w-full mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center gap-12 sm:gap-4 ">
             <div className="h-[50px] w-[206px]  relative">
               <Image src="/clients-1.png" fill alt="client 1 image" />
             </div>
